d7771dbaf1258ab0f9e62588325a4c7986d01087
Akamemeo <ralex9821@gmail.com>
2024-02-18 00:21:46 +0100

    hithubprueba

--- src/main/java/net/ube/testmod/TestMod.java
+++ src/main/java/net/ube/testmod/TestMod.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestMod implements ModInitializer {
-	public static final String MOD_ID = "testmod2";
+	public static final String MOD_ID = "testmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
